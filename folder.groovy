ce313d66489099ae8060c0e97f149192b3140c8f
rayinianji <anjaneyulu195@gmail.com>
2020-09-24 12:06:01 +0530

    added folder groovy

---
+++ folder.groovy
@@ -0,0 +1,16 @@
+folder('GERF-DEV') {
+    displayName('ABC')
+    description('Folder for project ABC')
+}
+listView('GERF-Project') {
+    description('All unstable jobs for project ABC')
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
