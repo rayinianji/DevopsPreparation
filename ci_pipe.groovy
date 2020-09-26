13ebe43d73ac6e1043f75fcfb927e21c890eecd9
rayinianji <anjaneyulu195@gmail.com>
2020-09-26 12:41:33 +0530

    added files

--- ci_pipe.groovy
+++ ci_pipe.groovy
@@ -1,4 +1,12 @@
-node
-{
+node{
+    stage('CheckOut code from SCM')
+    {
+        echo " Pulling the code for git SCM"
+    }
+    
+    stage('Building the code')
+    {
+        echo " Building the project"
+    }
     
 }
