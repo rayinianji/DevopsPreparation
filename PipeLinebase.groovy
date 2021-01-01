6b9c7f0dbcfd23d59cb437aa6630b166f63d9efb
rayinianji <anjaneyulu195@gmail.com>
2021-01-01 14:19:43 +0530

    updated

--- PipeLinebase.groovy
+++ PipeLinebase.groovy
@@ -7,3 +7,21 @@ pipelineJob('DevApp/ci-deploy') {
         }
     }
 }
+pipelineJob('DevApp/ci-deploy-QA') {
+    
+    definition {
+        cps {
+            script(readFileFromWorkspace('ci_pipe.groovy'))
+            sandbox()
+        }
+    }
+}
+pipelineJob('DevApp/ci-deploy-Prod') {
+    
+    definition {
+        cps {
+            script(readFileFromWorkspace('ci_pipe.groovy'))
+            sandbox()
+        }
+    }
+}
